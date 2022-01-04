d640115891230cb07c0e687e1230c2db15c29896
quanle28197 <86665723+quanle28197@users.noreply.github.com>
2022-01-04 14:02:35 +0700

    add

---
+++ ungdung.js
@@ -0,0 +1,6 @@
+let inputwidth = prompt('Enter the width');
+let inputheight = prompt('Enter the height');
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+let area = width * height;
+document.write('Area = ' + area);
